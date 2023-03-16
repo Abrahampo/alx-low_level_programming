c8bde1a7bab4c6a5f05489337ab9ea216e3ad566
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-16 10:45:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that print a line till a standard error
  * Return 1 (great)
  */
-int maim(void)
+int main(void)
 {	
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 return (1);
