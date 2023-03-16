881bed83c6677b5e52b01087f4664243cc955820
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-16 10:09:42 -0700

    6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - A Program that print a line using pf function
+* Return: 0 (great)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
