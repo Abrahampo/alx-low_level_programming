66c32b40f4925b7e9d6aa1d03f1d3670f377ab5d
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-18 13:39:00 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,43 +8,29 @@
 
 int main(void)
 {
-	int digit1 = 0, 
-	    digit2, digit3;
+	int digit = 0, digit2;
 
 	while (digit1 <= 9)
 	{
 		digit2 = 0;
 		while (digit2 <= 9)
 		{
-			digit3 = 0;
-			while(didgit3 <= 9)
+			if (digit1 != digit2 && digit1 < digit2)
 			{
-			       if (digit1 != digit2 &&
-			            digit1 < digit2 &&
-				    digit2 != digit3 &&
-				    digit2 < digit3)
-			{
-				           putchar(digit1 + 48);
-				           putchar(digit2 + 48);
-					   putchar(digit3 + 48);
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
 
-				if (digit1 + digit2 != 24)
+				if (digit1 + digit2 != 17)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			digit3++;
+			digit2++;
 		}
-		digit2++;
+		digit1++;
 	}
-	digit1++;
-	
-     }
-     putchar('\');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
-
- 
-
