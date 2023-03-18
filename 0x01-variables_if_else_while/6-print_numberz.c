55063245c2e4e8b6b06eb1f29fc873be81f558af
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-17 17:54:40 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entering digits
+ *
+ * Return: just 0 (succsee)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	putcahr(i);
+	putchar("\n");
+
+	return (0);
+}
