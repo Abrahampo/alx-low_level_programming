f0863437435d6d3a7de7f82640ed15248608282a
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-19 12:35:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
+#include <stdio.h>
+
 /**
- * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * main - UNIX is basically a simple operating system, but you have to be a gen
  * Return 1 (great)
  */
 int main(void)
-{	
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
