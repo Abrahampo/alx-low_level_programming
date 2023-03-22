cd4f738e3b933e432b562928e083fef6222cc37d
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-22 11:30:47 -0700

    0-main.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and print how days are 
+ * print_remaining_days - takes a date and print how days are
  * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
@@ -11,25 +11,25 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-	{
-		if (month > 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 -day);
-	}
-	else 
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("invalid date: %02d/%02d/%04d\n", month , day - 31, year);
-		}
-		else 
-		{
-			printf(" day of the year: %d\n", day);
-			printf("Remaining day: %d\n", 365 - day);
-		}
-	}
+        if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+        {
+                if (month > 2 && day >= 60)
+                {
+                        day++;
+                }
+                printf("day of the year: %d\n", day);
+                printf("Remaining days: %d\n", 366 - day);
+        }
+        else
+        {
+                if (month == 2 && day == 60)
+                {
+                        printf("invalid date: %02d/%02d/%04d\n", month , day - 31, year);
+                }
+                else
+                {
+                        printf(" day of the year: %d\n", day);
+                        printf("Remaining day: %d\n", 365 - day);
+                }
+        }
 }
