d2abf02a53e619b9359bb291bb5e777f7d727e14
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-23 02:06:32 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loop/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_lin - Draw a straigth line according to prameter
+ * @n: The number o lines to draw
+ * Retunr: empty
+ */
+
+void print_line(int n)
+
+{
+	int x;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_puctchar(95);
+		}
+		_putchar('\n');
+	}
+}
