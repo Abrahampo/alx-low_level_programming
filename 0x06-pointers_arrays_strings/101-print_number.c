da3308db01e60c00bf8c88c016267bf23753699d
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-30 12:18:01 -0700

    101-print_numbers.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,9 +15,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('_');
-		n1 = -n;
+		n1 = -n1;
 	}
-	if (n1 /  10 != 0)
+	if (n1 /  10 > 0)
 		print_number(n1 / 10);
 	_putchar((n1 % 10) + '0');
 }
