90fa1656a623d5230ec79125ff20a83ee53b6b9a
Abrahampo <abrahamgpoquee@gmail.com>
2023-03-30 12:31:27 -0700

    101-print_number.c is

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 	unsigned int n1;
 
 	n1 = n;
-	if (n < 0)
+	if (n > 0)
 	{
 		_putchar('_');
 		n1 = -n1;
 	}
 	if (n1 /  10 > 0)
 		print_number(n1 / 10);
-	_putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) - '0');
 }
