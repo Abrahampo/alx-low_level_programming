0f4eb76c1e37ce3ac56af913116b5f9319388694
Abrahampo <abrahamgpoquee@gmail.com>
2023-04-03 09:25:11 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 {
 	int k, n, pro1 = 0, pro2 = 0;
 
-	for (k = 0;k <= (size * size); k = k + size + 1)
+	for (k = 0; k <= (size * size); k = k + size + 1)
 		pro1 = pro1 + a[k];
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 		pro2 = pro2 + a[n];
