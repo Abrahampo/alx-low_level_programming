0ebd90c02c75c9b4eb8618975e36c4c12c5a0da1
Abrahampo <abrahamgpoquee@gmail.com>
2023-04-05 08:12:14 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#incluse "main.h"
+
+/**
+ * print_rev_recursion - prints a string in reverse
+ * in a reverse to a string
+ * Return: nothing.
+ */
+void _puts_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	-puchar(*s);
+}
